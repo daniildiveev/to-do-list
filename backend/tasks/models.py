44089780840e221b033603e8e2eb49a7d484c12c
Daniil.Diveev <daniildiveev@mail.ru>
2024-01-30 00:25:54 +0300

    feat: add Status model to tasks

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+class Status(models.Model):
+    name = models.CharField("name", max_length=15)
+    color = models.CharField("color", max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     title = models.CharField("name", max_length=100)
     creator = models.CharField("creator", max_length=100)
     description = models.CharField("description", max_length=1000)
-    status = models.CharField("status", max_length=20)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='tasks')
     created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField("date due")
 
