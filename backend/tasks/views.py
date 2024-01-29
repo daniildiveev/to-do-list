417bdb4e8e5440ac471cd5c512ef6e986de0d010
Daniil.Diveev <daniildiveev@mail.ru>
2024-01-29 18:34:17 +0300

    feat: implement rest for tasks

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .serializers import *
+from .models import Task
+
+
+@api_view(['POST', 'GET'])
+def tasks_list(request):
+    if request.method == 'GET':
+        data = Task.objects.all()
+        serializer = TaskSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def tasks_delete(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
